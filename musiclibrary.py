5cecd10e32b0c982f8fd1cd878c0f9b1df00c839
Gourab03042005 <itsgourab.rik@gmail.com>
2025-07-21 15:19:12 +0530

    Update musiclibrary.py

--- musiclibrary.py
+++ musiclibrary.py
@@ -4,7 +4,6 @@ music ={
     "sajni":"https://www.youtube.com/watch?v=k3g_WjLCsXM",
     "shape of you": "https://www.youtube.com/watch?v=JGwWNGJdvx8",
     "blinding lights": "https://www.youtube.com/watch?v=4NRXx6U8ABQ",
-     "Boba tunnel": "https://www.youtube.com/watch?v=gEmahl1XMB0&list=RDgEmahl1XMB0&start_radio=1",
      "Perfect": "https://www.youtube.com/watch?v=2Vv-BfVoq4g",
      "Kiska rasta dekhe": "https://www.youtube.com/watch?v=E0PBNCDACCU&list=RDE0PBNCDACCU&start_radio=1",
      "abhi na jao chhod kar":"https://www.youtube.com/watch?v=mfEQgoVi7P4&list=RDmfEQgoVi7P4&start_radio=1"
